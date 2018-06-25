985435ad7252eeb9b6ea78b3f2b125850bbad91d
Peggy Chiang <q375ml@gmail.com>
2018-06-25 23:49:45 +0800

    js

--- js/index.js
+++ js/index.js
@@ -18,36 +18,7 @@ $(function(){
 		$('html,body').animate({scrollTop:($(_href).offset().top-100)},1200)
 	})
 
-    //work
-     $('#w-d,#w-d2').animatedModal({
-        modalTarget:'modal-01',
-        animatedIn:'fadeInDown',
-        animatedOut:'fadeOutDown',
-        color:'#fff',
-        
-    });
-    $('#f-d,#f-d2').animatedModal({
-        modalTarget:'modal-02',
-        animatedIn:'fadeInDown',
-        animatedOut:'fadeOutDown',
-        color:'#fff',
-        
-        
-    });
-    $('#v-d,#v-d2').animatedModal({
-        modalTarget:'modal-03',
-        animatedIn:'fadeInDown',
-        animatedOut:'fadeOutDown',
-        color:'#fff',
-        
-    });
-    $('#u-d,#u-d2').animatedModal({
-        modalTarget:'modal-04',
-        animatedIn:'fadeInDown',
-        animatedOut:'fadeOutDown',
-        color:'#fff',
-        
-    });
+    
 
 
 
